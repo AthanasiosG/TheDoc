7602bf9748915bc33d5fdb9a6c0cde6369a88514
Athanasios Giotas <148756853+AthanasiosG@users.noreply.github.com>
2025-05-08 18:18:07 +0200

    Add main.py, commands.py and songs.py

---
+++ commands.py
@@ -0,0 +1,78 @@
+import discord
+import random
+from discord import app_commands
+from discord.ext import commands
+from songs import all_songs
+
+class BasicCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @app_commands.command(name="all_commands", description="Alle verfügbaren Bot-Commands")
+    async def all_commands(self, interaction: discord.Interaction):   
+        cmd_list = [cmd.name for cmd in self.bot.tree.get_commands()]
+        all_cmd = ""
+        for cmd in cmd_list:
+            all_cmd += "/" + cmd + "\n"
+        await interaction.response.send_message(embed=discord.Embed(title="Das sind alle verfügbaren Commands:", description=all_cmd, colour=6702))
+
+        
+    @app_commands.command(name="bot_info", description="Info zum Bot")
+    async def info_about_bot(self, interaction: discord.Interaction):
+        await interaction.response.send_message(embed=discord.Embed(title="Ich heiße DrBot und wurde von Thanos programmiert.", colour=6702))
+
+
+    @app_commands.command(name="server_owner", description="Gibt an wem der Server gehört")
+    async def server_owner(self, interaction: discord.Interaction):
+        owner = interaction.guild.owner
+        await interaction.response.send_message(embed=discord.Embed(title="Der Server gehört " + str(owner) +"!", colour=6702))
+
+        
+    @app_commands.command(name="kick", description="Einen User kicken")
+    @app_commands.checks.has_permissions(administrator=True)
+    async def kick(self, interaction: discord.Interaction, user: discord.User, reason: str):
+        await interaction.guild.kick(user=user, reason=reason)
+        await user.send(embed=discord.Embed(title="Du wurdest aus folgendem Grund gebannt:", description=reason, color=discord.Color.red()))
+        msg = f"{user} wurde erfolgreich gekickt!"
+        await interaction.user.send(embed=discord.Embed(title=msg, description="Grund: " + reason, color=discord.Color.red()))
+        await interaction.response.defer(ephemeral=True)        
+        await interaction.followup.send(f"Vorgang abgeschlossen.", ephemeral=True)
+
+
+    @app_commands.command(name="ban", description="Einen User bannen")
+    @app_commands.checks.has_permissions(administrator=True)
+    async def ban(self, interaction: discord.Interaction, user: discord.User, reason: str):
+        await interaction.guild.ban(user=user, reason=reason)
+        await user.send(embed=discord.Embed(title="Du wurdest aus folgendem Grund gebannt:", description=reason, color=discord.Color.red()))
+        msg = f"{user} wurde erfolgreich gebannt!"
+        await interaction.user.send(embed=discord.Embed(title=msg, description="Grund: " + reason, color=discord.Color.red()))
+        await interaction.response.defer(ephemeral=True)        
+        await interaction.followup.send(f"Vorgang abgeschlossen. User gebannt.", ephemeral=True)
+
+
+    @app_commands.command(name="rollen", description="Alle verfügbaren rollen")
+    async def server_rollen(self, interaction: discord.Interaction):
+        all_roles = [role.name for role in await interaction.guild.fetch_roles()]
+        await interaction.response.send_message(embed=discord.Embed(title="Alle Rollen", description=all_roles[::-1], colour=6702))          
+
+
+    @app_commands.command(name="clear", description="Löscht Nachrichten im Channel")
+    @app_commands.checks.has_permissions(administrator=True)
+    async def clear(self, interaction: discord.Interaction, amount: int):
+        await interaction.response.defer(ephemeral=True)
+        deleted = await interaction.channel.purge(limit=amount)
+        await interaction.followup.send(f"{len(deleted)} Nachrichten gelöscht.", ephemeral=True)
+
+
+    @app_commands.command(name="coinflip", description="Kopf oder Zahl")
+    async def coinflip(self, interaction: discord.Interaction):
+        coin = random.choice(["Kopf", "Zahl"])
+        coin = f"Du hast {coin} geworfen."
+        await interaction.response.send_message(embed=discord.Embed(title=coin, colour=6702))
+
+
+    @app_commands.command(name="songs", description="Alle verfügbaren Songs")
+    async def songs(self, interaction: discord.Interaction):
+        await interaction.response.send_message(embed=discord.Embed(title="Folgende Songs sind verfügbar:",
+                                                                    description="Fürs Aufrufen !+Songname benutzen:\n\nJust Danke\nPopular\nGive Me Everthing\nPoker Face\n",
+                                                                    colour=6702))
---
+++ main.py
@@ -0,0 +1,88 @@
+import discord
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+from commands import BasicCommands
+from songs import all_songs
+
+
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix="=", intents=intents)
+
+load_dotenv() 
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+
+
+@client.event
+async def on_ready():
+    await client.add_cog(BasicCommands(client))    
+    await client.tree.sync()
+
+
+@client.event
+async def on_guild_join(guild: discord.Guild):
+    all_channels = await guild.fetch_channels()
+    for channel in all_channels:
+        if channel.name in ["general", "chat", "allgemein"]:
+            await channel.send(embed=discord.Embed(title="Hallo! Ich bin TheDoc 😊", description="Mit /all_commands findet ihr alle verfügbaren Commands!", colour=6702))
+            break
+        
+        
+@client.event
+async def on_member_join(member):
+    if not member.bot:
+        await member.send(embed=discord.Embed(title="Willkommen auf dem Server", colour=6702))       
+    
+    
+@client.event
+async def on_voice_state_update(member, before, after):  
+    if before.channel is None and after.channel is not None:
+        channel = after.channel 
+        if len(channel.members) == 1:
+            cloned_channel = await channel.clone(name="Talk")
+            await cloned_channel.edit(name="Talk" + str(len(channel.guild.voice_channels)-1))
+            
+    elif before.channel is not None and after.channel is None and len(before.channel.members) == 0:
+        await before.channel.delete()
+        channels = before.channel
+        for i, channel in enumerate(channels.guild.voice_channels):
+            await channel.edit(name="Talk" + str(i))    
+
+
+@client.event
+async def on_reaction_add(reaction, user):
+    if not user.bot:
+        guild = reaction.message.guild
+        member = guild.get_member(user.id)
+
+        if reaction.emoji == "💎":
+            role = discord.utils.get(guild.roles, name="VIP")
+            if role:
+                await member.add_roles(role)
+        elif reaction.emoji == "◻️":
+            role = discord.utils.get(guild.roles, name="Member")
+            if role:
+                await member.add_roles(role)
+                
+                
+@client.event
+async def on_message(msg):
+    if msg.author.bot:
+        return       
+        
+    if msg.content in [song.lower() for song in all_songs.keys()] or msg.content in [song for song in all_songs.keys()]:    
+        song_name = ""
+        
+        for num, letter in enumerate(msg.content):
+            if num == 1:
+                song_name+= letter.upper()
+            else:
+                song_name+= letter
+                
+        for song, url in all_songs.items():
+            if msg.channel.name in ["general", "chat", "allgemein"] and song_name == song and msg.author != msg.author.bot:
+                await msg.reply(embed=discord.Embed(title=song[1::], url=url, colour=6702)) 
+                    
+                    
+client.run(TOKEN)
