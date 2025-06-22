d8631c763fe5aaaf695c963937c32cefa86ca8fc
Athanasios Giotas <148756853+AthanasiosG@users.noreply.github.com>
2025-06-22 23:21:14 +0200

    Improve code readability

--- src/bot_msg_manager.py
+++ src/bot_msg_manager.py
@@ -11,6 +11,7 @@ async def get_bot_message(user_id, msg_type="bot"):
     async with aiosqlite.connect("database.db") as conn:
         cursor = await conn.cursor()
         await cursor.execute("SELECT channel_id, message_id FROM bot_messages WHERE user_id=? AND type=?", (user_id, msg_type))
+       
         return await cursor.fetchone()
 
 async def delete_bot_message(user_id, msg_type="bot"):
--- src/main.py
+++ src/main.py
@@ -29,14 +29,17 @@ async def on_guild_join(guild: discord.Guild):
             embed = discord.Embed(title="Hello! I am TheDoc 😊", description="By using TheDoc, you agree to the terms of use and privacy policy. More info with /license. Use /all_commands to see all available commands!", colour=6702)
             embed = embed.set_thumbnail(url="https://raw.githubusercontent.com/AthanasiosG/TheDoc/main/images/thedoc.png")
             await channel.send(embed=embed)
+            
             async with aiosqlite.connect("database.db") as conn:
                 cursor = await conn.cursor()
                 await cursor.execute("INSERT OR IGNORE INTO auto_vc_control (guild_id, active) VALUES (?, ?)", (guild.id, 0))
             return
+        
         elif channel.name.lower() in ["general", "chat", "allgemein"]:
             embed = discord.Embed(title="Hello! I am TheDoc 😊", description="By using TheDoc, you agree to the terms of use and privacy policy. More info with /license. Use /all_commands to see all available commands!", colour=6702)
             embed = embed.set_thumbnail(url="https://raw.githubusercontent.com/AthanasiosG/TheDoc/main/images/thedoc.png")
             await channel.send(embed=embed)
+            
             async with aiosqlite.connect("database.db") as conn:
                 cursor = await conn.cursor()
                 await cursor.execute("INSERT OR IGNORE INTO auto_vc_control (guild_id, active) VALUES (?, ?)", (guild.id, 0))
@@ -47,9 +50,11 @@ async def on_guild_join(guild: discord.Guild):
 async def on_member_join(member):
     if not member.bot:
         guild = member.guild
+       
         async with aiosqlite.connect("database.db") as conn:
             cursor = await conn.cursor()
             await cursor.execute("DELETE FROM server_kicked WHERE user_id=?", (member.id,))
+       
         try:
             await member.send(embed=discord.Embed(title=f"Welcome to the server **{guild}**!", colour=6702)) 
         except discord.Forbidden:
@@ -65,6 +70,7 @@ async def on_member_remove(member):
     async with aiosqlite.connect("database.db") as conn:
         cursor = await conn.cursor()
         await cursor.execute("SELECT user_id FROM server_kicked")
+        
         for data in await cursor.fetchall():
             if data[0] == member.id:
                 is_user_kicked = True
@@ -73,6 +79,7 @@ async def on_member_remove(member):
         for channel in all_channels:
             if channel.name.lower() in ["willkommen", "welcome"] and isinstance(channel, discord.TextChannel):
                 invite = await channel.create_invite(max_age=86400, max_uses=1, unique=True)
+                
                 try:
                     await member.send(embed=discord.Embed(title=f"You have left **{guild}**:", description="If this was a mistake, you can rejoin using this link!", colour=6702, url=invite))
                     break
@@ -86,17 +93,21 @@ async def on_voice_state_update(member, before, after):
         cursor = await conn.cursor()
         await cursor.execute("SELECT active FROM auto_vc_control WHERE guild_id=?", (member.guild.id,))
         result = await cursor.fetchone()
+        
         if result is None or result[0] == 0:
             return
           
     if before.channel is None and after.channel is not None:
         channel = after.channel 
+    
         if len(channel.members) == 1:
             cloned_channel = await channel.clone(name="Talk")
             await cloned_channel.edit(name="Talk" + str(len(channel.guild.voice_channels)-1))      
+    
     elif before.channel is not None and after.channel is None and len(before.channel.members) == 0:
         await before.channel.delete()
         channels = before.channel
+    
         for i, channel in enumerate(channels.guild.voice_channels):
             await channel.edit(name="Talk" + str(i))    
                 
@@ -117,19 +128,25 @@ async def on_message(msg):
     if msg.content.startswith("!role_setup") and msg.author.guild_permissions.administrator:
         msg_list = msg.content.split()
         msg_list.pop(0)
+        
         async with aiosqlite.connect("database.db") as conn:
             cursor = await conn.cursor()
             await cursor.execute("DELETE FROM role_setup WHERE guild_id=?",(msg.guild.id,))
+            
             for role_emoji_pair in msg_list:
                 pair = ""
+                
                 for char in role_emoji_pair:
                     if char != ":":
                         pair += char
                     elif char == ":":
                         pair += " "
+                        
                 role_name, emoji = pair.split()
+                
                 await cursor.execute("INSERT OR IGNORE INTO role_setup VALUES (?, ?, ?)", (msg.guild.id, role_name, emoji))
                 await conn.commit()
+        
         await msg.channel.send("Data saved.", delete_after=5.0)
         await msg.delete()
            
@@ -138,29 +155,37 @@ async def on_message(msg):
             cursor = await conn.cursor()
             await cursor.execute("SELECT word FROM blacklist WHERE guild_id=?", (msg.guild.id,))
             blacklisted_words = [data[0] for data in await cursor.fetchall()]
+            
             await cursor.execute("SELECT amount FROM violation_limit WHERE guild_id=?", (msg.guild.id,))
             result = await cursor.fetchone()
             violation_limit = result[0] if result else 3
+            
             for content in msg.content.split():
                 if content in blacklisted_words:
                     await msg.delete()
+                    
                     guild_id, user_id = msg.guild.id, msg.author.id
                     await cursor.execute("INSERT INTO violation (guild_id, user_id) VALUES (?, ?)", (guild_id, user_id))
                     await conn.commit()
+                    
                     await cursor.execute("SELECT COUNT(*) FROM violation WHERE guild_id=? AND user_id=?", (guild_id, user_id))
                     violations = await cursor.fetchone()
                     violations = violations[0]
                     user = await msg.guild.fetch_member(user_id)
+                    
                     try:
                         if violations < violation_limit:
                             await user.send(embed=discord.Embed(title="WARNING!", description=f"If you violate again, you will be kicked from the server at {violation_limit} violations!",colour=6702))
+                        
                         elif violations == violation_limit:
                             await cursor.execute("INSERT INTO server_kicked (user_id) VALUES (?)", (user.id,))
                             await conn.commit()                             
                             await msg.guild.kick(user)
+                            
                             await user.send(embed=discord.Embed(title="KICKED!", description="You have been kicked due to too many violations!", color= discord.Color.red()))
                             await cursor.execute("DELETE FROM violation WHERE guild_id=? AND user_id=?", (msg.guild.id, user.id))
                             await conn.commit() 
+                            
                     except discord.Forbidden:
                         print("User has DMs disabled.")
 
